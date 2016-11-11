1f6cbff1fd21d90032bb4c5d04ea18f6324f4c0e
JaimeXD <jaime.obando@ucsp.edu.pe>
2016-11-11 08:05:03 -0500

    Update Poligono1.cpp

--- Poligono1.cpp
+++ Poligono1.cpp
@@ -1,16 +1,14 @@
 #include "poligono.h"
 #include "math.h"
+#include "punto.h"
 using namespace std;
 
-Poligono::Poligono(Point p[],int _size){
-    this->_size=size;
-    Pointarray a(p,_size);
-    
-    p=a;
+Poligono::Poligono(Point p[],int _size):points a(p,_size){
+    ++nro;
 
 }
-Poligono::Poligono(Pointarray &a){
-    p=Pointarray a;
+Poligono::Poligono(Pointarray &a):points (pa){
+    ++nro;
 }
 Poligono::getnumpoligonos(){
     return nro;
@@ -25,7 +23,15 @@ Poligono::getpoints(){
     return q;
 }
 
-
+Point constr [4];
+Point *ConstPoints ( const Point &p1 , const Point &p2 ,
+const Point &p3 , const Point &p4 = Point (0 ,0)) {
+    constr [0] = p1;
+    constr [1] = p2;
+    constr [2] = p3;
+    constr [3] = p4;
+    return constructorPoints ;
+}
 Rectangulo::Rectangulo(Point &p,Point &a):Poligono(p,Point(p.getx(),p.gety()),a,Point(a.getx(),a.gety()) ){
     
 }
